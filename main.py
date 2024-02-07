817d7c7f9fb81c7deb61972bbde60f0c22abd31b
jabar <jabari_mf@hotmail.com>
2024-02-06 21:56:43 -0600

    Some optimization has been done, however the outputs i think are wrong, will double check code after work

--- main.py
+++ main.py
@@ -1,32 +1,21 @@
 from collections import defaultdict
 
-
-#Depth First Search Algo
-#https://favtutor.com/blogs/depth-first-search-python explains it well
-
-#DFS Parameters (node, visited node, graph input)
-def dfs(node, visited, graph):
-    #redundancy check visted[node]
-    visited[node] = True
-    #for every v and direction in graph set [type will be dict]
-    for v, direction in graph[node]:
+def dfs(u, visited, graph, output):
+    visited[u] = True
+    for v, direction in graph[u]:
         if not visited[v]:
             if direction == 0:
                 direction = 1
-            dfs(v, visited, graph)
-            print(node, v, direction)
+            output.append((u, v, direction))
+            dfs(v, visited, graph, output)
 
 def main():
-    #first input n and m are for intersections and streets, this input controls them
     n, m = map(int, input().split())
 
     # Create graph
     graph = defaultdict(list)
     two_way_streets = []
 
-
-    #Optimzation oppurtunity, the code is specified to not care how many times the loop is run
-    # but by using "_", we can say the code just needs to run an unspecified amount of times
     for _ in range(m):
         a, b, c = map(int, input().split())
         if c == 1:
@@ -40,9 +29,20 @@ def main():
     visited = [False] * (n + 1)
 
     # Perform DFS to determine traffic directions
+    output = []
     for u in range(1, n + 1):
         if not visited[u]:
-            dfs(u, visited, graph)
+            dfs(u, visited, graph, output)
+
+    # Output directions for two-way streets
+    for street in two_way_streets:
+        a, b = street
+        direction = 2
+        for u, v, d in output:
+            if (u == a and v == b) or (u == b and v == a):
+                direction = d
+                break
+        print(a, b, direction)
 
 if __name__ == "__main__":
     main()
